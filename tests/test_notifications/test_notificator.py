f38cf7b05df38965403365abe4ea2a949edd325c
SomeExplorer <someexplorer@gmail.com>
2025-12-08 19:40:42 +0300

    Update tests

--- tests/test_notifications/test_notificator.py
+++ tests/test_notifications/test_notificator.py
@@ -1,6 +1,6 @@
-import time
+import asyncio
 from datetime import datetime, timedelta, timezone
-import threading
+from unittest.mock import AsyncMock
 
 import pytest
 from pytest_mock import MockerFixture
@@ -9,6 +9,8 @@ from apscheduler.schedulers.base import STATE_RUNNING, STATE_PAUSED
 from src.schemas.game_info_schema import GameInfoSchema
 from src.notifications.notificator import Notificator
 from src.notifications.scheduler import scheduler
+from src.notifications.models import Base, SentNotificationsModel
+from src.notifications.database import sync_engine
 from src.bots.base_bot import BaseBot
 
 TIME_DELTA = 5
@@ -19,6 +21,7 @@ test_game_json = {
     "description": "test_game_description",
     "keyImages": [{"type": "OfferImageWide", "url": "http://app.example.net"}],
     "seller": {"id": "test_seller_id", "name": "test_seller_name"},
+    "catalogNs": {"mappings": [{"pageSlug": "test_page_slug", "pageType": "test_page_type"}]},
     "offerMappings": [{"pageSlug": "test_page_slug", "pageType": "test_page_type"}],
     "price": {"totalPrice": {"discountPrice": 100, "originalPrice": 100, "discount": 0}},
     "promotions": {
@@ -39,13 +42,26 @@ test_game_json = {
 
 
 class Bot(BaseBot):
-    def notify_users(self, game: GameInfoSchema) -> None:
+    async def notify_users(self, game: GameInfoSchema) -> None:
         pass
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_db():
+    Base.metadata.create_all(sync_engine)
+    yield
+    Base.metadata.drop_all(sync_engine)
+
+
+@pytest.fixture()
+def clear_jobs():
+    scheduler.remove_all_jobs()
 
 
 class TestNotificator:
     notificator = Notificator(Bot())
 
+    @pytest.mark.usefixtures("clear_jobs")
     @pytest.mark.parametrize("test_games", [[GameInfoSchema(**test_game_json)], None])
     def test_check_store_update(self, test_games: list | None, mocker: MockerFixture):
         mocker.patch("src.notifications.notificator.get_free_games", return_value=test_games)
@@ -62,38 +78,39 @@ class TestNotificator:
                 end_date = test_game.promotions.upcoming_promotional_offers[0].promotional_offers[0].end_date
                 added_job = scheduler.get_job(current_jobs[-1].id)
                 assert added_job.args == (test_game,)
-                assert added_job.name == "notification"
+                assert added_job.name == test_game.title
                 assert added_job.trigger.run_date == start_date
                 assert added_job.misfire_grace_time < int((end_date - start_date).total_seconds())
                 added_job.remove()
 
-    def test_run(self):
-        thread1 = threading.Thread(target=self.notificator.run)
-        thread1.start()
-        time.sleep(0.5)
+    @pytest.mark.asyncio
+    @pytest.mark.usefixtures("clear_jobs")
+    @pytest.mark.parametrize("test_games", [[GameInfoSchema(**test_game_json)]])
+    async def test_run(self, test_games: list | None, mocker: MockerFixture):
+        mocker.patch("src.notifications.notificator.get_free_games", return_value=test_games)
+        mock_notify_users = mocker.patch(
+            "tests.test_notifications.test_notificator.Bot.notify_users", new_callable=AsyncMock
+        )
+
+        assert mock_notify_users is not None
+        run_task = asyncio.create_task(self.notificator.run())
+        await asyncio.sleep(7)
         assert scheduler.state == STATE_RUNNING
         assert self.notificator.running is True
-
-        first_run_job_date = scheduler.get_job("#0").next_run_time
-        first_run_jobs_len = len(scheduler.get_jobs())
+        first_run_updating_date = scheduler.get_job("#0").next_run_time
+        mock_notify_users.assert_called_once()
 
         self.notificator.running = False
-        thread1.join(timeout=1.0)
-        assert not thread1.is_alive()
+        await asyncio.sleep(1)
+        assert run_task.done()
         assert scheduler.state == STATE_PAUSED
 
-        thread2 = threading.Thread(target=self.notificator.run)
-        thread2.start()
-        time.sleep(0.5)
+        asyncio.create_task(self.notificator.run())
+        await asyncio.sleep(5)
         assert scheduler.state == STATE_RUNNING
         assert self.notificator.running is True
-
-        assert scheduler.get_job("#0").next_run_time == first_run_job_date
-        assert len(scheduler.get_jobs()) == first_run_jobs_len
-        self.notificator.running = False
-        thread2.join(timeout=1.0)
-        assert not thread2.is_alive()
-        assert scheduler.state == STATE_PAUSED
+        assert scheduler.get_job("#0").next_run_time == first_run_updating_date
+        mock_notify_users.assert_called_once()
 
 
 if __name__ == "__main__":
