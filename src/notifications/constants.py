24c0975b588433e0f7f4c2ad0d3ba61f4c206430
SomeExplorer <someexplorer@gmail.com>
2026-07-03 15:09:04 +0300

    refactor(API): change structure of API functions

--- src/notifications/constants.py
+++ src/notifications/constants.py
@@ -1,1 +1,2 @@
 UPDATING_TASK_ID = "#0"
+BASE_API_URL="https://store-site-backend-static.ak.epicgames.com"
--- src/notifications/epic_games_store_api.py
+++ src/notifications/epic_games_store_api.py
@@ -1,42 +1,37 @@
-import requests
+from requests import Session, RequestException, HTTPError
 from pydantic import ValidationError
 
+from src.logger import logger
 from src.schemas.store_schema import StoreSchema
 from src.schemas.game_info_schema import GameInfoSchema
+from src.notifications.constants import BASE_API_URL
 
 
-def get_store_data(
-    locale: str = "ru", country: str = "RU", allow_countries: str = "RU"
-) -> dict | None:
-    api_uri = (
-        "https://store-site-backend-static.ak.epicgames.com/"
-        f"freeGamesPromotions?locale={locale}&country={country}&allowCountries={allow_countries}"
-    )
-    with requests.Session() as session:
-        try:
-            response = session.get(api_uri)
-            response.raise_for_status()
-            data = response.json()
-            return data
-        except requests.exceptions.ConnectionError as e:
-            print(f"Connection error: {e}")
-        except requests.exceptions.Timeout as e:
-            print(f"Request timed out: {e}")
-        except requests.exceptions.RequestException as e:
-            print(f"An unexpected request error: {e}")
+def _get_store_data(locale: str = "ru", country: str = "RU", allow_countries: str = "RU") -> StoreSchema:
+    api_uri = f"{BASE_API_URL}/freeGamesPromotions?locale={locale}&country={country}&allowCountries={allow_countries}"
 
-
-def get_free_games(
-    locale: str = "ru", country: str = "RU", allow_countries: str = "RU"
-) -> list[GameInfoSchema] | None:
     try:
-        store_data = get_store_data(locale, country, allow_countries)
-        if not store_data:
-            return
-        store = StoreSchema(**get_store_data())
-    except ValidationError as e:
-        print(f"Validation error: {e}")
-    else:
-        free_games = store.data.catalog.search_store.elements
+        with Session() as session:
+            response = session.get(api_uri)
 
-        return free_games
+        if response.status_code != 200:
+            response.raise_for_status()
+            raise HTTPError(f"Unexpected status code {response.status_code}")
+
+        try:
+            validated_store_data = StoreSchema.model_validate(response.json())
+        except ValidationError as e:
+            logger.exception("Store schema is not correct: %s", str(e))
+            raise e
+        else:
+            return validated_store_data
+
+    except RequestException as e:
+        logger.exception("API request error: %s", str(e))
+        raise e
+
+
+def get_free_games(locale: str = "ru", country: str = "RU", allow_countries: str = "RU") -> list[GameInfoSchema]:
+    store_data = _get_store_data(locale, country, allow_countries)
+    free_games = store_data.data.catalog.search_store.elements
+    return free_games
